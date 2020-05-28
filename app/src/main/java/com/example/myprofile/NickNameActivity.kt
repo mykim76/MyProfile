3d565d47030cec36bf2e9eae6f678ce933ae4d44
mykim76 <iyoungfm@gmail.com>
2020-05-28 21:54:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myprofile/NickNameActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myprofile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NickNameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nick_name)
+    }
+}

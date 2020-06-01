7336b81bab456b74c70a279acea2cd7062da715d
mykim76 <iyoungfm@gmail.com>
2020-06-01 19:44:53 +0900

    전화연결 다이얼 연결까지

--- app/src/main/java/com/example/myprofile/MainActivity.kt
+++ app/src/main/java/com/example/myprofile/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.example.myprofile
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQ_MESSAGE_CODE = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+            getNickBtn.setOnClickListener {
+
+                val myIntent = Intent(this, NickNameActivity::class.java)
+                startActivityForResult(myIntent,REQ_MESSAGE_CODE)
+            }
+
+        dialBtn.setOnClickListener {
+            val phoneNum=phoneNumEdt.text.toString()
+            val myUri = Uri.parse("tel:${phoneNum}")
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+            startActivity(myIntent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+          if(requestCode == REQ_MESSAGE_CODE)
+          {
+              if(resultCode == Activity.RESULT_OK)
+              {
+                  val msg = data?.getStringExtra("message")
+                  nickTxt.text = msg
+
+                  Toast.makeText(this,"닉네임변경완료", Toast.LENGTH_SHORT).show()
+              }
+          }
     }
 }
--- app/src/main/java/com/example/myprofile/NickNameActivity.kt
+++ app/src/main/java/com/example/myprofile/NickNameActivity.kt
@@ -1,12 +1,26 @@
 package com.example.myprofile
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_nick_name.*
 
 class NickNameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nick_name)
+
+        goBackBtn.setOnClickListener {
+
+            val msg = nickNameEdt.text.toString()
+            val goBackIntent = Intent()
+            goBackIntent.putExtra("message",msg)
+            setResult(Activity.RESULT_OK,goBackIntent)
+
+            finish()
+        }
     }
 }
